783f4fc79fe53dc8bac560c6a59b1870777c098f
skoobyduubydoo <152900481+skoobyduubydoo@users.noreply.github.com>
2024-04-26 10:18:16 -0500

    Add files via upload

---
+++ Projects.java
@@ -0,0 +1,13 @@
+package projects;
+
+import projects.dao.DbConnection;
+
+public class Projects {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DbConnection.getConnection();
+		
+	}
+
+}
